0cbb0ffadd72f66a40cdeba15eeb63494a379762
Karol szykula <karol.szykula@gmail.com>
2018-12-13 10:03:45 +0100

    Finish render method

--- src/FetchUsers.js
+++ src/FetchUsers.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
 
 class FetchUsers extends Component {
+    state = {
+        data: null
+    }
+
+    componentDidMount() {
+        fetch("https://randomuser.me/api?results=5")
+            .then(response => response.json())
+            .then(data => this.setState({ data }));
+    }
+
     render() {
         return (
-            <div >
-                fetch
-        </div>
+            <div>
+                {this.state.data && this.state.data.results.map
+                    ? this.state.data.results.map(user => (
+                        <div>
+                            <div>
+                                <img src={user.picture.thumbnail} alt="user" />
+                            </div>
+                            <div>
+                                {user.name.first} {user.name.last}
+                            </div>
+                            <div>
+                                <a href="mailto:{user.email}">{user.email}</a>
+                            </div>
+                            <hr />
+                        </div>
+                    ))
+                    : "ladowanie"}
+            </div>
         )
     }
 }
